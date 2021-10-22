5daabe6310f5ee176e0be3ae15ee0415dc533952
Matthew Hirai <mhirai301@gmail.com>
2021-10-22 12:54:09 -0700

    separated cards, deck, and player into separate classes

---
+++ cards.py
@@ -0,0 +1,68 @@
+import random
+from player import Player
+
+
+class Cards:
+    def __init__(self):
+        self.deck = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', "K"]*4
+        for i in range(52):
+            randIndex = random.randint(0, 51)
+            temp = self.deck[i]
+            self.deck[i] = self.deck[randIndex]
+            self.deck[randIndex] = temp
+
+    def dealing_cards(self, cards, number):
+        for i in range(number):
+            if len(self.deck) == 0:
+                self.deck = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', "K"]*4
+                for i in range(52):
+                    randIndex = random.randint(0, 51)
+                    temp = self.deck[i]
+                    self.deck[i] = self.deck[randIndex]
+                    self.deck[randIndex] = temp
+
+            cards.append(self.deck.pop(0))
+        return cards
+
+    # conversion of player's cards
+    def convert(self, cards):
+        tens = ['J', 'Q', 'K']
+        t = 0
+        if cards[0] == 'A' and cards[1] == 'A':
+            cards[0] = 11
+            cards[1] = 1
+
+        for i in range(len(cards)):
+
+            if cards[i] in tens:
+                cards[i] = 10
+
+            elif cards[i] == 'A':
+                cards.pop(i)
+                cards_sum = self.convert(cards)
+                if cards_sum < 10:
+                    cards.append(11)
+
+                elif cards_sum >= 11:
+                    cards.append(1)
+
+                elif cards_sum == 10:
+                    cards.append(11)
+
+        t = sum(cards)
+        return t
+
+    # turns 1 and 11 to 'A' to show user
+    def ace(self, cards):
+        for n, i in enumerate(cards):
+            if i == 1 or i == 11:
+                cards[n] = 'A'
+        return cards
+
+    # adds a card to hand
+    def hit(self, cards):
+        cards = self.dealing_cards(cards, 1)
+        og_cards = cards.copy()
+        og_cards = self.ace(og_cards)
+        card_sum = self.convert(cards)
+        return cards, og_cards, card_sum

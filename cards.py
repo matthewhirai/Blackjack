2aa2f4727106b9985160c863da66316e942e5e83
Matthew Hirai <mhirai301@gmail.com>
2021-11-04 22:26:33 -0700

    added suits to cards

--- cards.py
+++ cards.py
@@ -3,7 +3,10 @@ import random
 
 class Cards:
     def __init__(self):
-        self.deck = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', "K"]*4
+        self.deck = ["A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠",
+                     "A♥", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥",
+                     "A♣", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣",
+                     "A♦", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦"]
         for i in range(52):
             randIndex = random.randint(0, 51)
             self.deck[i], self.deck[randIndex] = self.deck[randIndex], self.deck[i]
@@ -18,7 +21,10 @@ class Cards:
     def dealing_cards(self, cards, number):
         for i in range(number):
             if len(self.deck) == 0:
-                self.deck = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', "K"]*4
+                self.deck = ["A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠",
+                             "A♥", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥",
+                             "A♣", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣",
+                             "A♦", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦"]
                 for i in range(52):
                     randIndex = random.randint(0, 51)
                     self.deck[i], self.deck[randIndex] = self.deck[randIndex], self.deck[i]
@@ -29,25 +35,25 @@ class Cards:
     def convert(self, cards):
         tens = ['J', 'Q', 'K']
         t = 0
-        if cards[0] == 'A' and cards[1] == 'A':
+        if cards[0][:-1] == 'A' and cards[1][:-1] == 'A':
             t += 12
 
-        if cards[0] == 'A' and cards[1] != 'A':
+        if cards[0][:-1] == 'A' and cards[1][:-1] != 'A':
             cards[0], cards[1] = cards[1], cards[0]
 
         for i in range(len(cards)):
 
-            if cards[i] in tens:
+            if cards[i][:-1] in tens:
                 t += 10
 
-            elif cards[i] == 'A':
+            elif cards[i][:-1] == 'A':
                 if t <= 10:
                     t += 11
 
                 elif t >= 11:
                     t += 1
             else:
-                t += cards[i]
+                t += int(cards[i][:-1])
         return t
 
     # turns 1 and 11 to 'A' to show user
